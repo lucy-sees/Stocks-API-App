7eea8cd36501037f8e5b580715e2a3ab68a24eac
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-10-10 22:04:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom.js';
 import { Provider } from 'react-redux';
-import store from './redux/configureStore';
+// eslint-disable-next-line import/extensions
+import store from './redux/configureStore.js';
 import App from './App';
 
 const rootElement = document.getElementById('root');
