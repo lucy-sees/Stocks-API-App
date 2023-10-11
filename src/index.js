6a4b90ce8a83ed72c0e8f62daf19f6e9ce8c0216
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-10-11 19:13:13 +0300

    Update index.js to render NavBar

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-// eslint-disable-next-line import/extensions
-import store from './redux/configureStore.js';
+import reportWebVitals from './reportWebVitals';
+import store from './redux/configureStore';
+import NavBar from './components/NavBar';
 import App from './App';
 
 const rootElement = document.getElementById('root');
 
-ReactDOM.unstable_createRoot(rootElement).render(
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <BrowserRouter>
+      <NavBar />
       <Provider store={store}>
         <App />
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 );
+
+reportWebVitals();
