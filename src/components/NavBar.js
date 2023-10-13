29d40ac844bc0e392dff9b2d09ef5ca262d7b4d6
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-10-13 18:11:01 +0300

    Update NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import { NavLink } from 'react-router-dom';
 import { FaArrowLeft } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
 import NavBarStyle from './NavBar.module.css';
 
 const NavBar = () => (
   <nav className={NavBarStyle.navContent}>
     <NavLink key={uuidv4()} to="/home">
-      <FaArrowLeft name="left-arrow" type="solid" color="#f7f7f7" />
+      <FaArrowLeft name="left-arrow" type="solid" color="#f8f8f8" />
     </NavLink>
     <div>
       <NavLink key={uuidv4()} to="/home" className={NavBarStyle.navLink}>Home</NavLink>
