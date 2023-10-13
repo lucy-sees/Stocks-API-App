f46a2796074083891939ef4192de1dfceac35a64
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-10-13 18:11:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import reportWebVitals from './reportWebVitals';
 import store from './redux/configureStore';
-import NavBar from './components/NavBar';
 import App from './App';
 
-const rootElement = document.getElementById('root');
-
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <NavBar />
       <Provider store={store}>
         <App />
       </Provider>
@@ -20,5 +15,3 @@ ReactDOM.createRoot(rootElement).render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-reportWebVitals();
