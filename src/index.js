b2db93a5e70c26cd01365d91065763dec3367dab
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-10-14 01:35:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
+import NavBar from './components/NavBar';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
     <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <NavBar />
+      <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </Provider>,
 );
