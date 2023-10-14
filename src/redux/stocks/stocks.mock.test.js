c1f3c1f3745d733a9444a65f7ba5df9a9df5c25d
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-10-14 16:23:44 +0300

    Add a mock test for the API fetch

---
+++ src/redux/stocks/stocks.mock.test.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import axios from 'axios';
+
+const API_KEY = '17fb152bc0303cf17ad11b8b6fae087a';
+const STOCKS_API_URL = `https://financialmodelingprep.com/api/v3/stock/list?apikey=${API_KEY}`;
+
+const fetchData = async () => {
+  const url = STOCKS_API_URL;
+  const getURL = await axios.get(url);
+  return getURL;
+};
+
+fetchData('react');
+
+describe('Testing API axios', () => {
+  test('URL is the correct', () => {
+    const url = 'https://financialmodelingprep.com/api/v3/stock/list?apikey=17fb152bc0303cf17ad11b8b6fae087a';
+    expect(url).toStrictEqual(STOCKS_API_URL);
+  });
+});
+
+export { fetchData, STOCKS_API_URL };
